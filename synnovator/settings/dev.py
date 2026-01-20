a23ba1ceee7c4dc323541e97c3e4a1d767caa112
Allen Woods <allen.woods@outlook.com>
2026-01-20 13:40:33 +0800

    Init wagtail project and setup claude

---
+++ synnovator/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-!#qz6senm70#0g6pqpwy3pmsr70w!-k6@78e2#7)&-^-)(y+2e"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
